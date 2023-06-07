6b0a4816fae319df4047fa186b0e4151aaa14c65
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-07 16:45:28 -0600

    Agregue el archivo TipoVariable

---
+++ Java JRE y JDK compile y ejecute su primer programa/TipoVariable.java
@@ -0,0 +1,13 @@
+
+public class TipoVariable {
+	public static void main (String[] args) {
+		int edad = 28;
+		System.out.println(edad);
+		
+		edad = 48;
+		System.out.println(edad);
+		
+		edad = 26 + 30;
+		System.out.println("Mi edad es de " + edad);
+	}
+}
