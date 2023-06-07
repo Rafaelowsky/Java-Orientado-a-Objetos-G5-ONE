ccab2afa4c3b78080e9475d4589bc2f6413bc109
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-07 17:17:51 -0600

    Agregué código y tambien lo comenté

--- Java JRE y JDK compile y ejecute su primer programa/TipoVariable.java
+++ Java JRE y JDK compile y ejecute su primer programa/TipoVariable.java
@@ -1,13 +1,28 @@
 
 public class TipoVariable {
 	public static void main (String[] args) {
+		//"int" sirve para declarar variables enteras
 		int edad = 28;
+		//con el siguiente comando podemos imprimir en consola
 		System.out.println(edad);
 		
 		edad = 48;
 		System.out.println(edad);
 		
 		edad = 26 + 30;
+		//Gracias a el simbolo + podemos concatenar variables con strings
 		System.out.println("Mi edad es de " + edad);
+		
+		//Con double declaramos variables con varios decimales,
+		//aunque el resultado sea entero puede ocuparse la variable declarada como double
+		//pero lo adecuado para optimizar el programa es utilizar el tipo de variable adecuada
+		double salario = 28;
+		double salarioMitad = salario/2;
+		System.out.println(salarioMitad);
+		
+		//Al declarar un variable entera y su resultado sea fraccionaria, este se va a redondear
+		int division = 1250 / 3;
+		System.out.println(division);
+		
 	}
 }
