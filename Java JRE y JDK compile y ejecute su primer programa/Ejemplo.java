3b16a41c239ae20bc0d4a1497cc62703195fdab2
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-07 15:48:21 -0600

    Cambie Ejemplo y configure eclipse/git

--- Java JRE y JDK compile y ejecute su primer programa/Ejemplo.java
+++ Java JRE y JDK compile y ejecute su primer programa/Ejemplo.java
@@ -1,5 +1,7 @@
-public class Ejemplo{
-    public static void main(String[] args){
-        System.out.println("Hola Mundo");
-    }
+
+public class Ejemplo {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
 }
