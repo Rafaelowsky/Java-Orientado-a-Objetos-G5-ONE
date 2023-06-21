40ad46de6056d071e1278ce4249e5022413e1d23
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-21 14:34:05 -0600

    comente multiplicación.java

--- Java JRE y JDK compile y ejecute su primer programa/Multiplicacion.java
+++ Java JRE y JDK compile y ejecute su primer programa/Multiplicacion.java
@@ -1,7 +1,7 @@
 
 public class Multiplicacion {
 	public static void main(String[] args) {
-		
+		//Se pueden hacer iteraciones sobre iteraciones haciendolas anidadas.
 		for (int contador = 0; contador <= 10; contador++) {
 			for (int multiplicador = 0; multiplicador <= 10; multiplicador++) {
 				System.out.print(contador * multiplicador);
