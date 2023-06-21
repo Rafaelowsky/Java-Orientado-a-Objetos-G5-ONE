f653e7ec83f2acd7c79c2ae44af7e43f75c318a0
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-20 21:40:09 -0600

    Agregue EjemplosCicloJava y modifique EjemplosCiclos2

--- Java JRE y JDK compile y ejecute su primer programa/EjemplosCiclos2.java
+++ Java JRE y JDK compile y ejecute su primer programa/EjemplosCiclos2.java
@@ -2,6 +2,9 @@
 public class EjemplosCiclos2 {
 	public static void main(String[] args) {
 
+		//Con esto podemos hacer un ciclo donde se este modificando
+		//un programa con el que con la iteracion de while, modificamos
+		//el valor de una variable e imprimirla al final
 		int contador = 0;
 		int total = 0;
 		while (contador <= 10) {
---
+++ Java JRE y JDK compile y ejecute su primer programa/EjemplosCiclosFor.java
@@ -0,0 +1,10 @@
+
+public class EjemplosCiclosFor {
+	public static void main(String[] args) {
+		
+		//    declaración		condición		incrementador
+		for (int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+	}
+}
