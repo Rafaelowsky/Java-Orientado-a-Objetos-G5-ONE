59dafa2d3e33d1027e75e18955ec154f68e6f8cd
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-21 13:14:29 -0600

    Agregué Multiplicación.java

---
+++ Java JRE y JDK compile y ejecute su primer programa/Multiplicacion.java
@@ -0,0 +1,13 @@
+
+public class Multiplicacion {
+	public static void main(String[] args) {
+		
+		for (int contador = 0; contador <= 10; contador++) {
+			for (int multiplicador = 0; multiplicador <= 10; multiplicador++) {
+				System.out.print(contador * multiplicador);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
