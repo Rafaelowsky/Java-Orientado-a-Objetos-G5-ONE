1e3dab32aafd867b00e8b510900e3d9d3368a2bb
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-21 20:35:06 -0600

    Modifique las iteraciones

--- Java JRE y JDK compile y ejecute su primer programa/Multiplicacion.java
+++ Java JRE y JDK compile y ejecute su primer programa/Multiplicacion.java
@@ -2,8 +2,8 @@
 public class Multiplicacion {
 	public static void main(String[] args) {
 		//Se pueden hacer iteraciones sobre iteraciones haciendolas anidadas.
-		for (int contador = 0; contador <= 10; contador++) {
-			for (int multiplicador = 0; multiplicador <= 10; multiplicador++) {
+		for (int contador = 1; contador <= 10; contador++) {
+			for (int multiplicador = 1; multiplicador <= 10; multiplicador++) {
 				System.out.print(contador * multiplicador);
 				System.out.print(" ");
 			}
