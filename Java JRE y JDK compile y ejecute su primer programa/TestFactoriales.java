4f4b3762653623cfcb7b7101f8edcca10d6d1cc1
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-22 00:55:11 -0600

    Agregué TestFactoriales

---
+++ Java JRE y JDK compile y ejecute su primer programa/TestFactoriales.java
@@ -0,0 +1,11 @@
+
+public class TestFactoriales {
+
+    public static void main(String[] args) {
+        int factorial = 1;
+        for (int i = 1; i <= 10; i++) {
+            factorial *= i;
+            System.out.println("Factorial de " + i + " = " + factorial);
+        }
+    }
+}
