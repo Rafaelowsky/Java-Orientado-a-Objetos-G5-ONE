26fd406e35d9b5587a5a8c33d616c42d67d5a95e
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-22 15:12:54 -0600

    Añadí codigo y comente a CrearCuenta.java y TestReferencia.java

--- JavaOO_Entendiendo_Orientacion_a_Objetos/src/CrearCuenta.java
+++ JavaOO_Entendiendo_Orientacion_a_Objetos/src/CrearCuenta.java
@@ -3,6 +3,8 @@ public class CrearCuenta {
 	public static void main(String[] args) {
 		//Con esto llamamos a la clase cuenta para poder generar datos con ella
 		//con el comando "new" hacemos referencia a hacer espacio en la memoria para recibir nuevos datos
+		//Variable       ->      Valor
+		//Variable       ->     12384DA
 		Cuenta primeraCuenta = new Cuenta();
 		primeraCuenta.saldo = 1000;
 		System.out.println(primeraCuenta.saldo);
@@ -10,5 +12,26 @@ public class CrearCuenta {
 		Cuenta segundaCuenta = new Cuenta();
 		segundaCuenta.saldo = 500;
 		System.out.println(segundaCuenta.saldo);
+		
+		primeraCuenta.saldo = 500;
+		//Con esto podemos ver que ID's tienen los objetos
+		System.out.println(primeraCuenta);
+		System.out.println(segundaCuenta);
+		
+		//Con esto podemos ver que aunque tienen los mismos atributos
+		//tienen diferentes IDs
+		if(primeraCuenta == segundaCuenta) {
+			System.out.println("Son los mismos ID's");
+		} else {
+			System.out.println("No son los mismos ID's");
+		}
+		
+		//Y aunque tengan diferentes atributos pero pueden contener
+		//los mismos atributos independientes entre si
+		if (primeraCuenta.saldo == segundaCuenta.saldo) {
+			System.out.println("Tienen los mismos atributos");
+		} else {
+			System.out.println("No tienen los mismos atributos");
+		}
 	}
 }
---
+++ JavaOO_Entendiendo_Orientacion_a_Objetos/src/TestReferencia.java
@@ -0,0 +1,24 @@
+
+public class TestReferencia {
+ public static void main(String[] args) {
+	//Aqui estamos declarando otra cuenta, pero por lo visto se genera
+	//un ID diferente al que ya teniamos declarado para primeraCuenta
+	Cuenta primeraCuenta = new Cuenta();
+	primeraCuenta.saldo = 100;
+	//Al declarar esto se genera que los ID's de segunda y primer 
+	//Cuenta sean los mismos y estos comparten los mismos atributos
+	Cuenta segundaCuenta = primeraCuenta;
+	System.out.println(primeraCuenta.saldo);
+	System.out.println(segundaCuenta.saldo);
+	
+	
+	if(primeraCuenta == segundaCuenta) {
+		System.out.println("Son los mismos ID's");
+	} else {
+		System.out.println("No son los mismos ID's");
+	}
+	
+	System.out.println(primeraCuenta);
+	System.out.println(segundaCuenta);
+}
+}
