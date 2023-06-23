b2dac96e30540c3a266d77dd0073e7f08d641bde
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-22 18:04:50 -0600

    Agregué código para probar "trasferir" y agregue comprobación de transf.

--- JavaOO_Entendiendo_Orientacion_a_Objetos/src/PruebaMetodos.java
+++ JavaOO_Entendiendo_Orientacion_a_Objetos/src/PruebaMetodos.java
@@ -6,7 +6,18 @@ public class PruebaMetodos {
 		miCuenta.depositar(300);
 		System.out.println(miCuenta.saldo);
 		
-		miCuenta.retirar(700);
+		miCuenta.retirar(100);
+		System.out.println(miCuenta.saldo);
+		
+		Cuenta cuentaJimena = new Cuenta ();
+		cuentaJimena.depositar(1000);
+		
+		if (cuentaJimena.transferir(400, miCuenta)) {
+			System.out.println("Transferencia exitosa");
+		} else { System.out.println("Imposible de realizar"); 
+		
+		}
+		System.out.println(cuentaJimena.saldo);
 		System.out.println(miCuenta.saldo);
 	}
 }
