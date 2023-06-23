f187f5afdb2411ad7bc531ef8513114da4022c05
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-22 18:03:33 -0600

    Agregué método transferir y cambie operadores en métodos anteriores

--- JavaOO_Entendiendo_Orientacion_a_Objetos/src/Cuenta.java
+++ JavaOO_Entendiendo_Orientacion_a_Objetos/src/Cuenta.java
@@ -9,15 +9,26 @@
 		//No retorna valor (void) 
 		public void depositar(double valor) {
 		   // object       object 
-			this.saldo = this.saldo + valor;
+			this.saldo += valor;
 		}
 		
-		//Retorna valor
+		//Retorna valor booleano
 		public boolean retirar (double valor) {
 			if (this.saldo >= valor) {
-				this.saldo = this.saldo - valor;
+				this.saldo -= valor;
 				return true;
 			} else { System.out.println("No puedes retirar mas del dinero que tienes");
+				return false;
+			}
+		}
+		
+		// Retorna valor booleano
+		public boolean transferir (double valor, Cuenta cuenta) {
+			if (this.saldo >= valor) {
+				this.saldo -= valor;
+				cuenta.depositar(valor);
+				return true;
+			} else {
 				return false;
 			}
 		}
