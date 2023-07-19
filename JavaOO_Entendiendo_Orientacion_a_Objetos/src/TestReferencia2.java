36c8f354929b637ead3a294d1f8cf6cda1e3e504
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-18 20:42:39 -0600

    Modifique getters y setters

--- JavaOO_Entendiendo_Orientacion_a_Objetos/src/TestReferencia2.java
+++ JavaOO_Entendiendo_Orientacion_a_Objetos/src/TestReferencia2.java
@@ -3,13 +3,13 @@ public class TestReferencia2 {
 	public static void main(String[] args) {
 		
 	Cliente diego = new Cliente(); 
-	diego.nombre = "diego";
-	diego.documento = "319055539";
-	diego.telefono = "55 71202614";
+	diego.setNombre("diego");
+	diego.setDocumento("319055539");
+	diego.setTelefono("55 71202614");
 	
 	Cuenta cuentaDiego = new Cuenta();
 	cuentaDiego.titular = diego;
-	System.out.println(cuentaDiego.titular.telefono);
+	System.out.println(cuentaDiego.titular.getTelefono());
 	}
 	
 }
--- JavaOO_Entendiendo_Orientacion_a_Objetos/src/TestReferencia3.java
+++ JavaOO_Entendiendo_Orientacion_a_Objetos/src/TestReferencia3.java
@@ -2,7 +2,7 @@
 public class TestReferencia3 {
 	public static void main(String[] args) {
 		Cuenta cliente = new Cuenta();
-		cliente.titular.nombre = "diego";
-		System.out.println(cliente.titular.nombre);
+		cliente.titular.setNombre("diego");
+		System.out.println(cliente.titular.getNombre());
 	}
 }
